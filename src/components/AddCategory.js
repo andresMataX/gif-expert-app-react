dc4c9246b8ed51c1806580abe7646e8f210abbb3
Andrés Mata <andres_mata21@outlook.com>
2022-04-14 23:10:51 -0500

    Manejo del input en la categoría

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const AddCategory = () => {
+
+    const [inputValue, setInputValue] = useState('Hola mundo');
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Submit hecho');
+    }
+
     return (
-        <>
-            <input type="text" />
-        </>
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
+            />
+        </form>
     )
 }
