67740ea7097f6099d95abd959ec9c102cec550c9
Andrés Mata <andres_mata21@outlook.com>
2022-04-15 15:38:45 -0500

    Obtención de los gifs desde la API

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,27 @@
 import React from 'react'
 
 export const GifGrid = ({ category }) => {
+
+    const getGifs = async () => {
+        const url = 'https://api.giphy.com/v1/gifs/search?q=My Chemical Romance&limit=10&api_key=E7TOz8YH0UdvnAm3I6kt7CdpH7o5McoY';
+
+        const resp = await fetch(url);
+
+        const { data } = await resp.json();
+
+        const gifs = data.map(img => {
+            return {
+                id: img.id,
+                title: img.title,
+                url: img.images?.downsized_medium.url
+            }
+        });
+
+        console.log(gifs);
+    }
+
+    getGifs();
+
     return (
         <div>
             <h3>{category}</h3>
