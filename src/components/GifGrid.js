5928464b51593be879bea5bf8ee6355459388f1e
Andrés Mata <andres_mata21@outlook.com>
2022-04-16 01:15:53 -0500

    Uso de un custom hook para obtener los gifs

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,19 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import { getGifs } from '../helpers/getGifs';
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
+// import { getGifs } from '../helpers/getGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
-    const [images, setImages] = useState([]);
+    // const [images, setImages] = useState([]);
 
-    // Utilizamos este hook para no volver a llamar la función cuando se vuelve a renderizar el componente por usar useState
-    useEffect(() => {
-        getGifs(category).then(setImages);
-    }, [category]); // Se le pasa como segundo parámetro la lista de dependencias, al ser vacío, solamente se ejecuta una sola vez
+    const { data: images, loading } = useFetchGifs(category);
+
+    /*// Utilizamos este hook para no volver a llamar la función cuando se vuelve a renderizar el componente por usar useState
+    // useEffect(() => {
+    //     getGifs(category).then(setImages);
+    // }, [category]); // Se le pasa como segundo parámetro la lista de dependencias, al ser vacío, solamente se ejecuta una sola vez
     // Si la categoría cambia, pasa a ejecutar este efecto para mandar la llamada a la API.
+    */
+
     return (
         <>
             <h3>{category}</h3>
+            {loading && <p>Loading</p>}
             <div className='card-grid'>
                 {
                     images.map((image) => {
